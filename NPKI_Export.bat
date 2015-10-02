a447ff3b8f31853043bb2c0d57f7059afa5040cc
Lee, SungUk <sulee@ea.com>
2015-10-02 20:03:54 +0900

    Support compress and encrypt NPKI datas
    Add external binaries
    - 7z
    - gnupg
    
    bat file force change linefeed

--- NPKI_Export.bat
+++ NPKI_Export.bat
@@ -1,15 +1,21 @@
 ::window clear
 @echo off
-title NPKI복사기
+title "NPKI복사기"
 
 ::Clear window
 cls
 
 echo "공인인증서(NPKI)를 복사하는 배치파일입니다"
-echo C드라이브의 공인인증서(NPKI) 복사를 하시려면 아무키나 눌러주세요"
-pause 
-mkdir "%USERPROFILE%\Desktop\NPKI"
-xcopy "%USERPROFILE%\AppData\LocalLow\NPKI" "%USERPROFILE%\Desktop\NPKI" /e /h /k
+echo "C드라이브의 공인인증서(NPKI) 복사를 하시려면 아무키나 눌러주세요"
+pause
+mkdir "NPKI"
+xcopy "%USERPROFILE%\AppData\LocalLow\NPKI" "NPKI" /e /h /k
 cls
-echo "복사를 완료하였습니다. 바탕화면의 NPKI폴더를 확인하여주세요"
+extern\7z\7za.exe a NPKI.zip "NPKI"
+rmdir "NPKI" /s /q
+extern\gnupg\gpg2.exe -o NPKI.zip.gpg --cipher-algo AES256 --compression-algo BZIP2 -c npki.zip
+del NPKI.zip
+extern\gnupg\gpg-connect-agent.exe killagent
+echo "복사를 완료하였습니다. NPKI.zip.gpg 파일을 복사하세요"
+
 pause
--- NPKI_Install.bat
+++ NPKI_Install.bat
@@ -6,10 +6,14 @@ title NPKI설치
 cls
 
 echo "공인인증서(NPKI)를 설치하는 배치파일입니다"
-echo 바탕화면의 백업된 NPKI폴더를 투척하신후 공인인증서(NPKI) 설치를 하시려면 아무키나 눌러주세요"
-pause 
+echo "설치하시려면 NPKI.zip.gpg 를 복사하신 후 아무키나 눌러주세요"
+pause
+extern\gnupg\gpg2.exe -o NPKI.zip -d NPKI.zip.gpg
+extern\gnupg\gpg-connect-agent.exe killagent
+extern\7z\7za.exe x NPKI.zip
 mkdir "%USERPROFILE%\AppData\LocalLow\NPKI"
-xcopy "%USERPROFILE%\Desktop\NPKI" "%USERPROFILE%\AppData\LocalLow\NPKI" /e /h /k
+xcopy "NPKI" "%USERPROFILE%\AppData\LocalLow\NPKI" /e /h /k
+del NPKI.zip
 cls
 echo "설치를 완료하였습니다. 오늘도 좋은하루 보내시길 바랍니다 고갱님."
 pause
