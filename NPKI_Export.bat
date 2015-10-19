d5fafb667a59feed3081e86b14edbca37b188b9e
Sunguk Lee <d3m3vilurr@gmail.com>
2015-10-20 02:51:15 +0900

    Support old version windows

--- NPKI_Export.bat
+++ NPKI_Export.bat
@@ -5,11 +5,14 @@ title "NPKI복사기"
 ::Clear window
 cls
 
+ver | find /i "version 6." > nul
+if %errorlevel%==0 (set TARGET=%USERPROFILE%\AppData\LocalLow) else (set TARGET=C:\Program files)
+
 echo "공인인증서(NPKI)를 복사하는 배치파일입니다"
 echo "C드라이브의 공인인증서(NPKI) 복사를 하시려면 아무키나 눌러주세요"
 pause
 mkdir "NPKI"
-xcopy "%USERPROFILE%\AppData\LocalLow\NPKI" "NPKI" /e /h /k
+xcopy "%TARGET%\NPKI" "NPKI" /e /h /k
 cls
 extern\7z\7za.exe a NPKI.zip "NPKI"
 rmdir "NPKI" /s /q
--- NPKI_Install.bat
+++ NPKI_Install.bat
@@ -5,14 +5,17 @@ title NPKI설치
 ::Clear window
 cls
 
+ver | find /i "version 6." > nul
+if %errorlevel%==0 (set TARGET=%USERPROFILE%\AppData\LocalLow) else (set TARGET=C:\Program files)
+
 echo "공인인증서(NPKI)를 설치하는 배치파일입니다"
 echo "설치하시려면 NPKI.zip.gpg 를 복사하신 후 아무키나 눌러주세요"
 pause
 extern\gnupg\gpg2.exe -o NPKI.zip -d NPKI.zip.gpg
 extern\gnupg\gpg-connect-agent.exe killagent
 extern\7z\7za.exe x NPKI.zip
-mkdir "%USERPROFILE%\AppData\LocalLow\NPKI"
-xcopy "NPKI" "%USERPROFILE%\AppData\LocalLow\NPKI" /e /h /k
+mkdir "%TARGET%\NPKI"
+xcopy "NPKI" "%TARGET%\NPKI" /e /h /k
 del NPKI.zip
 cls
 echo "설치를 완료하였습니다. 오늘도 좋은하루 보내시길 바랍니다 고갱님."
